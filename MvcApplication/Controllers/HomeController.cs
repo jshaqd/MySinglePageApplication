8f193c3a770c8b22f3cc9fb1ccdae0288b795de2
Angela Qian <Angela.qiandan@gmail.com>
2013-01-26 13:14:27 -0600

    Initial commit

---
+++ MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using MvcApplication.Models;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //var mongoDb = MongoDatabase.Create(ConfigurationManager.AppSettings["MongoDBTimesheets"]);
+            //var repository = mongoDb.GetCollection<Timesheet>(typeof(Timesheet).Name);
+            //var timesheets = new List<Timesheet>
+            //{
+            //    new Timesheet { FirstName = "Christophe", LastName = "Geers", Month = 8, Year = 2012},
+            //    new Timesheet { FirstName = "Ruben", LastName = "Geers", Month = 8, Year = 2012 }
+            //};
+            //foreach (var timesheet in timesheets)
+            //    repository.Insert(timesheet);
+
+            //repository.RemoveAll();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
